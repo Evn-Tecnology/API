bed3167091df7d8a236f1a99f055c3c0885db8e8
shum3l <ayenglem1@upao.edu.pe>
2024-09-19 05:41:23 -0500

    EV-42 Registro de Usuarios

---
+++ Event-Tecnology/src/main/java/com/TecnologyGroup/Event_Tecnology/model/dto/UserRequestDTO.java
@@ -0,0 +1,34 @@
+package com.TecnologyGroup.Event_Tecnology.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDTO {
+    @NotBlank(message = "El nombre no puede ser vacío")
+    @Size(min = 2, message = "El nombre debe tener al menos 2 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede ser vacío")
+    @Size(min = 2, message = "El apellido debe tener al menos 2 caracteres")
+    private String apellido;
+
+    @NotBlank(message = "El correo electrónico no puede ser vacío")
+    @Email(message = "El correo debe ser válido")
+    private String email;
+
+    @Pattern(regexp = "^[+]?[0-9][\\s\\-()][0-9]+$", message = "El número de teléfono puede contener solo dígitos, espacios, guiones, paréntesis y un signo '+' opcional")
+    private String telefono;
+
+    @NotBlank(message = "la contraseña no puede ser vacío")
+    @Size(min = 8, max = 20, message = "La contraseña debe tener entre 8 y 20 caracteres")
+    @Pattern(regexp = "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[!@#$%^&+=]).*$", message = "La contraseña debe contener al menos un número, una letra minúscula, una letra mayúscula y un carácter especial")
+    private String password;
+}
---
+++ Event-Tecnology/src/main/java/com/TecnologyGroup/Event_Tecnology/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.TecnologyGroup.Event_Tecnology.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_nombre", nullable = false)
+    private String userNombre;
+
+    @Column(name = "user_apellido", nullable = false)
+    private String userApellido;
+
+    @Column(name = "user_email", nullable = false, unique = true, updatable = false)
+    private String userEmail;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    @Column(name = "user_telefono")
+    private String userTelefono;
+
+    @Column(name = "email_verified", nullable = false)
+    private boolean emailVerified = false;
+}
