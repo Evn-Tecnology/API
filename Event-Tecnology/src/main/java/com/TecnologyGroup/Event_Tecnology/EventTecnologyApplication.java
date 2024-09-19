db38df79aaec066d2734924b6b94d5b55190944b
CarlosMorales <cmoralesc7@upao.edu.pe>
2024-09-19 04:47:19 -0500

    Estructuración Inicial

---
+++ Event-Tecnology/src/main/java/com/TecnologyGroup/Event_Tecnology/EventTecnologyApplication.java
@@ -0,0 +1,13 @@
+package com.TecnologyGroup.Event_Tecnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTecnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTecnologyApplication.class, args);
+	}
+
+}
---
+++ Event-Tecnology/src/test/java/com/TecnologyGroup/Event_Tecnology/EventTecnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TecnologyGroup.Event_Tecnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTecnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
