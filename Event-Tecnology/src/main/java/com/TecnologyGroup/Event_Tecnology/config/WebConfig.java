93acd01cfec68926b8542e6e8c1686615f376a40
CarlosMorales <cmoralesc7@upao.edu.pe>
2024-11-04 01:57:59 -0500

    Se agrego clase config para la conexion con el front

---
+++ Event-Tecnology/src/main/java/com/TecnologyGroup/Event_Tecnology/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.TecnologyGroup.Event_Tecnology.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Cambia esto si el frontend está en otro dominio
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
